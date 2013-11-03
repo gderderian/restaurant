a01da9c43d9487c61352cc276fae807314a052d4
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-02 22:25:11 -0700

    Making changes to allow tests to work properly with lab JUnit testing code

--- src/restaurant/test/mock/MockCustomer.java
+++ src/restaurant/test/mock/MockCustomer.java
@@ -1,7 +1,12 @@
 package restaurant.test.mock;
 
 
-import restaurant.interfaces.Cashier;
+import java.util.ArrayList;
+
+import restaurant.CashierAgent;
+import restaurant.Menu;
+import restaurant.WaiterAgent;
+import restaurant.interfaces.Waiter;
 import restaurant.interfaces.Customer;
 
 /**
@@ -15,7 +20,9 @@ public class MockCustomer extends Mock implements Customer {
 	/**
 	 * Reference to the Cashier under test that can be set by the unit test.
 	 */
-	public Cashier cashier;
+	public Customer customer;
+	public ArrayList<LoggedEvent> log = new ArrayList<LoggedEvent>();
+	public CashierAgent cashier;
 
 	public MockCustomer(String name) {
 		super(name);
@@ -23,31 +30,13 @@ public class MockCustomer extends Mock implements Customer {
 	}
 
 	@Override
-	public void HereIsYourTotal(double total) {
-		log.add(new LoggedEvent("Received HereIsYourTotal from cashier. Total = "+ total));
-
-		if(this.name.toLowerCase().contains("thief")){
-			//test the non-normative scenario where the customer has no money if their name contains the string "theif"
-			cashier.IAmShort(this, 0);
-
-		}else if (this.name.toLowerCase().contains("rich")){
-			//test the non-normative scenario where the customer overpays if their name contains the string "rich"
-			cashier.HereIsMyPayment(this, Math.ceil(total));
-
-		}else{
-			//test the normative scenario
-			cashier.HereIsMyPayment(this, total);
-		}
+	public void dispenseChange(double newMoney) {
+		log.add(new LoggedEvent("Received message dispenseChange. Change=" + newMoney));
 	}
-
+	
 	@Override
-	public void HereIsYourChange(double total) {
-		log.add(new LoggedEvent("Received HereIsYourChange from cashier. Change = "+ total));
-	}
-
-	@Override
-	public void YouOweUs(double remaining_cost) {
-		log.add(new LoggedEvent("Received YouOweUs from cashier. Debt = "+ remaining_cost));
+	public void hereIsCheck(double amountDue) {
+		log.add(new LoggedEvent("Received message hereIsCheck. AmountDue=" + amountDue));
 	}
 
 }
