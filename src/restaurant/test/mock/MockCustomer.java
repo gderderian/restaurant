13f6c585e96dbd5406ada03a0c2152500e13a728
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-03 15:28:11 -0800

    Correcting accessor functions in mockfiles

--- src/restaurant/test/mock/MockCustomer.java
+++ src/restaurant/test/mock/MockCustomer.java
@@ -4,8 +4,10 @@ package restaurant.test.mock;
 import java.util.ArrayList;
 
 import restaurant.CashierAgent;
+import restaurant.HostAgent;
 import restaurant.Menu;
 import restaurant.WaiterAgent;
+import restaurant.gui.CustomerGui;
 import restaurant.interfaces.Waiter;
 import restaurant.interfaces.Customer;
 
@@ -37,6 +39,79 @@ public class MockCustomer extends Mock implements Customer {
 	@Override
 	public void hereIsCheck(double amountDue) {
 		log.add(new LoggedEvent("Received message hereIsCheck. AmountDue=" + amountDue));
+	}
+
+	@Override
+	public void gotHungry() {
+		log.add(new LoggedEvent("Received message gotHungry."));
+		
+	}
+
+	@Override
+	public void msgSitAtTable(Menu m, Waiter w) {
+		log.add(new LoggedEvent("Received message msgSitAtTable."));
+		
+	}
+
+	@Override
+	public void msgWhatDoYouWant() {
+		log.add(new LoggedEvent("Received message msgWhatDoYouWant."));
+	}
+
+	@Override
+	public void hereIsOrder(String choice) {
+		log.add(new LoggedEvent("Received message hereIsOrder. choice=" + choice));
+	}
+
+	@Override
+	public void repickFood(Menu newMenu) {
+		log.add(new LoggedEvent("Received message repickFood along with a new menu."));
+		
+	}
+
+	@Override
+	public void goToCorner() {
+		log.add(new LoggedEvent("Received action goToCorner."));
+	}
+
+	@Override
+	public void restaurantFull() {
+		log.add(new LoggedEvent("Received action restaurantFull."));
+	}
+
+	@Override
+	public String pickRandomItem() {
+		log.add(new LoggedEvent("Received action pickRandomItem."));
+		return null;
+	}
+
+	@Override
+	public String pickRandomItemWithinCost() {
+		log.add(new LoggedEvent("Received action pickRandomItemWIthinCost."));
+		return null;
+	}
+
+	@Override
+	public void releaseSemaphore() {
+		log.add(new LoggedEvent("Received action releaseSemaphore."));
+	}
+
+	@Override
+	public String getCustomerName() {
+		log.add(new LoggedEvent("Received call to getCustomerName"));
+		return null;
+	}
+
+	@Override
+	public CustomerGui getGui() {
+		log.add(new LoggedEvent("Received call to getGui."));
+		return null;
+	}
+
+	@Override
+	public HostAgent getHost() {
+		log.add(new LoggedEvent("Received call to getHost."));
+		return null;
 	}
 
 }
--- src/restaurant/test/mock/MockWaiter.java
+++ src/restaurant/test/mock/MockWaiter.java
@@ -1,7 +1,12 @@
 package restaurant.test.mock;
 
 
-import restaurant.CustomerAgent;
+import restaurant.CashierAgent;
+import restaurant.CookAgent;
+import restaurant.HostAgent;
+import restaurant.Menu;
+import restaurant.WaiterAgent.MyCustomer;
+import restaurant.gui.WaiterGui;
 import restaurant.interfaces.Customer;
 import restaurant.interfaces.Waiter;
 
@@ -27,13 +32,94 @@ public class MockWaiter extends Mock implements Waiter {
 	}
 	
 	@Override
-	public void hereIsCheck(CustomerAgent c, double checkAmount) {
+	public void hereIsCheck(Customer c, double checkAmount) {
 		log.add(new LoggedEvent("Received message hereIsCheck in amount " + checkAmount));
 	}
 	
 	@Override
-	public void readyForCheck(CustomerAgent c){
+	public void readyForCheck(Customer c){
 		log.add(new LoggedEvent("Received message readyForCheck."));
+	}
+
+	@Override
+	public void doneEating(Customer c) {
+		log.add(new LoggedEvent("Received message doneEating."));
+	}
+
+	@Override
+	public void hereIsFood(int tableNum, String choice) {
+		log.add(new LoggedEvent("Received message hereIsFood."));
+	}
+
+	@Override
+	public void msgSeatCustomer(Customer c, int tableNum, HostAgent h, int customerX, int customerY) {
+		log.add(new LoggedEvent("Received message msgSeatCustomer."));
+	}
+
+	@Override
+	public void readyToOrder(Customer c) {
+		log.add(new LoggedEvent("Received message readyToOrder."));
+	}
+
+	@Override
+	public void hereIsMyChoice(String choice, Customer c) {
+		log.add(new LoggedEvent("Received message hereIsMyChoice."));
+	}
+
+	@Override
+	public void ImDone(Customer c) {
+		log.add(new LoggedEvent("Received message ImDone."));
+	}
+
+	@Override
+	public void needNewChoice(int tableNum, String choice) {
+		log.add(new LoggedEvent("Received message needNewChoice."));
+	}
+
+	@Override
+	public void breakApproved() {
+		log.add(new LoggedEvent("Received message breakApproved."));
+	}
+
+	@Override
+	public void breakRejected() {
+		log.add(new LoggedEvent("Received message breakRejected."));
+	}
+
+	@Override
+	public void requestBreak() {
+		log.add(new LoggedEvent("Received message requestBreak."));
+	}
+
+	@Override
+	public void seatCustomer(MyCustomer c) {
+		log.add(new LoggedEvent("Received message seatCustomer."));
+	}
+
+	@Override
+	public void deliverOrder(MyCustomer c, String choice) {
+		log.add(new LoggedEvent("Received message deliverOrder."));
+	}
+
+	@Override
+	public void goodbyeCustomer(MyCustomer c) {
+		log.add(new LoggedEvent("Received message goodbyeCustomer."));
+	}
+
+	@Override
+	public void releaseSemaphore() {
+		log.add(new LoggedEvent("Received message releaseSemaphore."));
+	}
+
+	@Override
+	public boolean hasCustomer(Customer c) {
+		log.add(new LoggedEvent("Received call to hasCustomer."));
+		return false;
+	}
+
+	@Override
+	public void notifyHostReturnedFromBreak() {
+		log.add(new LoggedEvent("Received call to notifyHostReturnedFromBreak."));
 	}
 
 }
